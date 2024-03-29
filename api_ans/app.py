b2b824d1b152f9c6a5a1e16439452142453bce98
luizcortezdev <luizcortezcontato@gmail.com>
2024-03-28 20:18:51 -0400

    commit 6

--- api_ans/app.py
+++ api_ans/app.py
@@ -6,7 +6,7 @@ import psycopg2
 app = FastAPI()
 
 conn = psycopg2.connect(
-    dbname='ans',
+    dbname='',
     user='',
     password='',
     host='' #O Endpoint do Banco RDS da AWS
@@ -28,5 +28,22 @@ async def buscar_operadoras(query: str = Query(None)):
     cursor.close()
     return resultados
 
+@app.get('/buscar-nome/{nome_operadora}')
+async def buscar_operadora_por_nome(nome_operadora: str):
+    cursor = conn.cursor()
+    cursor.execute("SELECT cnpj, razao_social FROM operadoras WHERE razao_social iLIKE %s", ('%' + nome_operadora + '%',))
+    resultados = [{'CNPJ': row[0], 'nome': row[1]} for row in cursor.fetchall()]
+    cursor.close()
+    return resultados
+
+@app.get('/buscar-registro/{registro_ans}')
+async def buscar_operadora_por_registro_ans(registro_ans: int):
+    cursor = conn.cursor()
+    cursor.execute("SELECT cnpj, razao_social FROM operadoras WHERE registro_ans = %s", (registro_ans,))
+    resultados = [{'CNPJ': row[0], 'nome': row[1]} for row in cursor.fetchall()]
+    cursor.close()
+    return resultados
+
+
 if __name__ == '__main__':
-    uvicorn.run(app, host="127.0.0.1", port=80)
+    uvicorn.run(app, host="127.0.0.1", port=5000)
