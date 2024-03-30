653f8f8ba34e2946a8851fe39d488b5b7233d13c
luizcortezdev <luizcortezcontato@gmail.com>
2024-03-30 16:50:26 -0400

    troca s3

--- api_ans/IMPORTANTE.md
+++ api_ans/IMPORTANTE.md
@@ -1,3 +1,4 @@
+
 Link para a api: https://bit.ly/operadoras_ans
 
 Documentação da API se encontra em /docs do site!!
--- api_ans/app.py
+++ api_ans/app.py
@@ -1,9 +1,15 @@
-from fastapi import FastAPI, Query, File
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, Query, HTTPException
+from fastapi.responses import HTMLResponse
 import uvicorn
 import psycopg2
+import boto3
 
-app = FastAPI()
+s3_client = boto3.client( # conexao com a interface vue.js por um bucket s3
+    service_name='s3',
+    aws_access_key_id='',
+    aws_secret_access_key='',
+    region_name='us-east-1' 
+)
 
 conn = psycopg2.connect(
     dbname='',
@@ -12,12 +18,18 @@ conn = psycopg2.connect(
     host='' #O Endpoint do Banco RDS da AWS
 )
 
-@app.get('/')
+app = FastAPI()
+
+@app.get('/') # Rota Interface Vue.js para Bucket S3
 async def index():
-    return FileResponse('static/index.html')
+    s3_bucket_name = "api-fastapi"
+    response = s3_client.get_object(Bucket=s3_bucket_name, Key='index.html')
+    html_content = response['Body'].read().decode('utf-8')
+
+    return HTMLResponse(content=html_content, status_code=200)
 
 
-@app.get('/buscar-operadoras')
+@app.get('/buscar-operadoras') # Rota para busca textual em interface vue.js
 async def buscar_operadoras(query: str = Query(None)):
     if query is None:
         return []
@@ -28,7 +40,7 @@ async def buscar_operadoras(query: str = Query(None)):
     cursor.close()
     return resultados
 
-@app.get('/buscar-nome/{nome_operadora}')
+@app.get('/buscar-nome/{nome_operadora}') # Rota API para busca textual - retorna json
 async def buscar_operadora_por_nome(nome_operadora: str):
     cursor = conn.cursor()
     cursor.execute("SELECT cnpj, razao_social FROM operadoras WHERE razao_social iLIKE %s", ('%' + nome_operadora + '%',))
@@ -36,7 +48,7 @@ async def buscar_operadora_por_nome(nome_operadora: str):
     cursor.close()
     return resultados
 
-@app.get('/buscar-registro/{registro_ans}')
+@app.get('/buscar-registro/{registro_ans}') # Rota API para busca por registro_ans - retorna json
 async def buscar_operadora_por_registro_ans(registro_ans: int):
     cursor = conn.cursor()
     cursor.execute("SELECT cnpj, razao_social FROM operadoras WHERE registro_ans = %s", (registro_ans,))
